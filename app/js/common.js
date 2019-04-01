2f4cfe3b50ff460a07e3981d23d25b15386dac15
Your Name <9leon1@gmail.com>
2019-04-01 21:55:53 +0300

    finish @media

--- app/js/common.js
+++ app/js/common.js
@@ -1,73 +1,9 @@
-$(function () {
-
-// slick slider
-
-    $(document).ready(function () {
-        $(".slideshow").slick({
-            dots: false,
-            arrows: false
-        });
-        $(".menu-slider a").click(function (e) {
-            e.preventDefault();
-            slideIndex = $(this).index();
-            $('.slideshow').slickGoTo(parseInt(slideIndex));
-        });
-    });
-
-// counter
-
-    function catalogItemCounter(field) {
-        var fieldCount = function (el) {
-            var min = el.data('min') || false,
-                max = el.data('max') || false,
-                dec = el.next('.dec'),
-                inc = el.prev('.inc');
-
-            function init(el) {
-                if (!el.attr('disabled')) {
-                    dec.on('click', decrement);
-                    inc.on('click', increment);
-                }
-
-                function decrement() {
-                    var value = parseInt(el[0].value);
-                    value--;
-
-                    if (!min || value >= min) {
-                        el[0].value = value;
-                    }
-                }
-
-                function increment() {
-                    var value = parseInt(el[0].value);
-                    value++;
-                    if (!max || value <= max) {
-                        el[0].value = value++;
-                    }
-                }
-            }
-
-            el.each(function () {
-                init($(this));
-            });
-        };
-
-        $(field).each(function () {
-            fieldCount($(this));
-        });
-    }
-
-    catalogItemCounter('.fieldCount');
 
 // menuToggle
 
-    $('.menuToggle').on('click', function () {
-        $('.menu').slideToggle(300, function () {
-            if ($(this).css('display') === "none") {
-                $(this).removeAttr('style');
-            }
-        });
-    });
+$('.menu-toggle').click(function () {
+    $(this).toggleClass('active');
+    $('.main-menu').slideToggle(400);
 });
 
 //Tabs
@@ -80,23 +16,6 @@ $('.tabs a').click(function () {
     $(this).parent().addClass('active');
     return false
 });
-
-
-
-//sort in work
-
-// var nodelist = document.querySelectorAll('.portfolio-list__item');
-// var arr = Array.from(nodelist);
-//
-// console.log( arr );
-//
-// arr.reverse();
-// console.log( arr );
-//
-//
-// document.getElementsByClassName('portfolio-list')[0].innerHTML = arr;
-// var content = document.getElementsByClassName('portfolio-list').innerHTML;
-
 
 //slider
 
